b7c1a574694dc34e32f96076b7d7073ea3b6b4ac
EnWei <10646007@ntub.edu.tw>
2020-08-21 19:35:56 +0800

    新增Entity - CreditCardBalance

---
+++ modules/camping-banker-database-config/src/main/java/tw/edu/ntub/imd/camping/banker/databaseconfig/dao/CreditCardBalanceDAO.java
@@ -0,0 +1,9 @@
+package tw.edu.ntub.imd.camping.banker.databaseconfig.dao;
+
+import org.springframework.stereotype.Repository;
+import tw.edu.ntub.imd.camping.banker.databaseconfig.entity.view.CreditCardBalance;
+
+@Repository
+public interface CreditCardBalanceDAO extends BaseDAO<CreditCardBalance, String> {
+
+}
