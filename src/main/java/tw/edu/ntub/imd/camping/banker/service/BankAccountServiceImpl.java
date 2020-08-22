07f644f2794d452fbd319e5b93b80e4882e01a0c
EnWei <10646007@ntub.edu.tw>
2020-08-22 09:29:23 +0800

    修正 - 重複新增銀行帳號時會出現未知錯誤

--- src/main/java/tw/edu/ntub/imd/camping/banker/service/BankAccountServiceImpl.java
+++ src/main/java/tw/edu/ntub/imd/camping/banker/service/BankAccountServiceImpl.java
@@ -1,9 +1,11 @@
 package tw.edu.ntub.imd.camping.banker.service;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import tw.edu.ntub.imd.camping.banker.bean.BankAccountBean;
 import tw.edu.ntub.imd.camping.banker.databaseconfig.dao.BankAccountDAO;
 import tw.edu.ntub.imd.camping.banker.databaseconfig.entity.BankAccount;
+import tw.edu.ntub.imd.camping.banker.exception.DuplicateCreateException;
 import tw.edu.ntub.imd.camping.banker.service.transformer.BankAccountTransformer;
 
 @Service
@@ -19,7 +21,11 @@ public class BankAccountServiceImpl extends BaseServiceImpl<BankAccountBean, Ban
 
     @Override
     public BankAccountBean save(BankAccountBean bankAccountBean) {
-        BankAccount saveResult = bankAccountDAO.save(transformer.transferToEntity(bankAccountBean));
-        return transformer.transferToBean(saveResult);
+        try {
+            BankAccount saveResult = bankAccountDAO.save(transformer.transferToEntity(bankAccountBean));
+            return transformer.transferToBean(saveResult);
+        } catch (DataIntegrityViolationException e) {
+            throw new DuplicateCreateException("此銀行帳戶已經存在");
+        }
     }
 }
