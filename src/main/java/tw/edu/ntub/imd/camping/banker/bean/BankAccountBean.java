c906bd3e4fba398431d4a106e5e702b698c48530
EnWei <10646007@ntub.edu.tw>
2020-08-23 16:26:54 +0800

    修正 - 銀行帳戶為10 ~ 16碼而非固定16碼

--- src/main/java/tw/edu/ntub/imd/camping/banker/bean/BankAccountBean.java
+++ src/main/java/tw/edu/ntub/imd/camping/banker/bean/BankAccountBean.java
@@ -4,13 +4,14 @@ import lombok.Data;
 import tw.edu.ntub.imd.camping.banker.databaseconfig.enumerate.BankType;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.PositiveOrZero;
 import javax.validation.constraints.Size;
 
 @Data
 public class BankAccountBean {
     @NotBlank(message = "帳號 - 未填寫")
-    @Size(max = 16, message = "帳號 - 輸入字數大於{max}個字")
+    @Pattern(regexp = "^[0-9]{10,16}$", message = "帳號 - 應為10到16個字")
     private String account;
     @Size(max = 3, message = "銀行代號 - 輸入字數大於{max}個字")
     private String bankId;
